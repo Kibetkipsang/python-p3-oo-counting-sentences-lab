388f5a09220352161cb3a1c0b13c1dfad352fb74
Kibet Kipsang <denniskipsang2000@gmail.com>
2025-02-17 17:17:27 +0300

    count_sentences.py

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,42 @@
-#!/usr/bin/env python3
+
+import re
+import sys
+import io
 
 class MyString:
-  pass
+  def __init__(self, value=''):
+    self.set_value(value)
+
+  def set_value(self, value):
+    if not isinstance(value, str): 
+            self._value = ""
+            print("The value must be a string.")
+    else:
+        self._value = value
+
+  @property
+  def value(self):
+    return self._value
+
+  @value.setter
+  def value(self, value):
+    self.set_value(value)
+
+  def is_sentence(self):
+    return self.value.endswith('.')
+  
+  def is_question(self):
+    return self.value.endswith('?')
+  
+  def is_exclamation(self):
+    return self.value.endswith('!')
+  
+  def count_sentences(self):
+    if not isinstance(self.value, str):
+            return 0
+    sentences = re.split(r'[.!?]+', self.value.strip())
+    return len([s for s in sentences if s]) 
+
+
+string = MyString()
+string.value = 123 
